e5884bbba3ae683d9d82e557141cdb621d48d0b2
Ryan J. Geyer <me@ryangeyer.com>
2013-09-16 21:17:02 -0700

    No more python version conditional

--- setup.py
+++ setup.py
@@ -5,19 +5,14 @@ from setuptools import find_packages
 import fitparse
 
 
-requires = None
-if sys.version_info < (2, 7):
-    requires = ['argparse']
-
-
-    setup(
-        name='fitparse',
-        version='0.0.1',
-        description='Python library to parse ANT/Garmin .FIT files',
-        author='David Cooper',
-        author_email='dave@kupesoft.com',
-        url='https://www.github.com/dtcooper/python-fitparse',
-        license=open('LICENSE').read(),
-        packages=find_packages(),
-        install_requires=requires,
-        )
+setup(
+    name='fitparse',
+    version='0.0.1',
+    description='Python library to parse ANT/Garmin .FIT files',
+    author='David Cooper',
+    author_email='dave@kupesoft.com',
+    url='https://www.github.com/dtcooper/python-fitparse',
+    license=open('LICENSE').read(),
+    packages=find_packages(),
+    install_requires=requires,
+    )
