ce0136714cb5df1d7ecdb8d56deb043be334f126
Ryan J. Geyer <me@ryangeyer.com>
2013-09-16 20:54:19 -0700

    Added a version

--- fitparse/__init__.py
+++ fitparse/__init__.py
@@ -1,4 +1,5 @@
 from fitparse.activity import Activity
 from fitparse.exceptions import FitError, FitParseError, FitParseComplete
 
+__version__ = '0.0.1'
 __all__ = ['Activity', 'FitError', 'FitParseError', 'FitParseComplete']
